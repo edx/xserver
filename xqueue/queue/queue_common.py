4a0385e1e790cc0ec8cedd95ce64fc884ac9ff99
kimth <kimth@stanford.edu>
2012-08-07 23:21:25 +0000

    Remove cruft from queue_common.py

--- xqueue/queue/queue_common.py
+++ xqueue/queue/queue_common.py
@@ -1,13 +1,2 @@
-#
-# Xqueue header/body/footer convention is as follows:
-#
-#   Header: Routing information (callback id and secret key) between 
-#               LMS <--> Xqueue, and Xqueue <--> External grader
-#   Body:   Serialized message between LMS <--> External grader
-#               (Contents will NOT be modified by Xqueue)
-#
-HEADER_TAG = 'xqueue_header'
-BODY_TAG   = 'xqueue_body'
-
 RABBIT_HOST = 'localhost'
 QUEUES = ['MITx-6.00x', 'python', 'null']
